a6dfe351dce53c9f075d03d262c8209d7295a644
harjot362 <124993744+harjot362@users.noreply.github.com>
2025-03-03 15:50:47 +0530

    Updated changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import Header from "./components/Header";
 import Slider from "./components/Slider";
 import Subscription from "./components/Subscription";
 import Footer from "./components/Footer";
+import Home from "./components/Home";
 
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
       <Slider/>
       <Subscription/>
       <Footer/>
